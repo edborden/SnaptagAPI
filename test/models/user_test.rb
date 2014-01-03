9a7ccb615df3c69040bd59695d35cce71b80f7fe
Ed Borden <borden.edward@gmail.com>
2014-01-02 19:44:54 -0500

    getting testing going

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "users are created and default to inactive" do
+  	user=users(:user1) 	assert_equal user.active, false
+ 	assert_equal user.name, nil
+  end
+
 end
