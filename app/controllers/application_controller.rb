9e7e680fff49bb6cc65816d0f8b292cd0ded4650
Ed Borden <borden.edward@gmail.com>
2014-03-22 16:45:04 -0400

    trying to fix cors again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 	def cors_set_access_control_headers
 		headers['Access-Control-Allow-Origin'] = '*'
 		headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-		headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+		headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With Authorization X-CSRF-Token}.join(',')
 		headers['Access-Control-Max-Age'] = "1728000"
 	end
 
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::API
 		if request.method == "OPTIONS"
 			headers['Access-Control-Allow-Origin'] = '*'
 			headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-			headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+			headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With Authorization X-CSRF-Token}.join(',')
 			headers['Access-Control-Max-Age'] = '1728000'
 			render :text => '', :content_type => 'text/plain'
 		end
