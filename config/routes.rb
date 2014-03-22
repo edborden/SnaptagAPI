28bb05274a0494b4a3396cacf5e0fe9ef96bc1a3
Ed Borden <borden.edward@gmail.com>
2014-03-21 21:08:21 -0400

    testing format constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Gridwebclient::Application.routes.draw do
 	#constraints :format => "json" do
-		post 'users', to: 'users#create', format: :json
+		post 'users', to: 'users#create', :constraints => { :format => 'json' } 
 		get 'users', to: 'users#show'
 	#end
 end
