844f1f52e5a4a89e0cce958d78c4d68b7dfc514c
Ed Borden <borden.edward@gmail.com>
2014-04-08 13:18:22 -0400

    user.active?

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Gridwebclient::Application.routes.draw do
 
 	get 'users/login', to: 'users#login'
 	get 'users/me', to: 'users#me'
+	get 'users/active', to: 'users#active'
 	get 'users', to: 'users#find'
 
 end
