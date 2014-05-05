05974e26e1232a67a6b7c9d79644fec10209764c
Ed Borden <borden.edward@gmail.com>
2014-05-05 14:26:39 -0400

    test service

--- test/fabricators/fabricator.rb
+++ test/fabricators/fabricator.rb
@@ -3,8 +3,8 @@ Fabricator(:user) do
 	lastname { Faker::Name.last_name }
   	email { |attrs| "#{Faker::Lorem.characters(10)}@example.com" }
   	active true
-  	token { |attrs| "#{Faker::Lorem.characters(30)}" }
-  	facebookid { |attrs| "#{Faker::Number.number(30)}" }
+  	token { |attrs| "#{Faker::Lorem.characters(10)}" }
+  	facebookid { |attrs| "#{Faker::Number.number(10)}" }
   	smallpic "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/p50x50/75022_10
 152135585049906_64496435_t.jpg" 
 	mediumpic "https://fbcdn-photos-e-a.akamaihd.net/hphotos-ak-prn1/t1.0-0/75022_101521355
