e8465ffd6812b36b862f7e3e9689486cf1d49dfa
Ed Borden <borden.edward@gmail.com>
2014-05-10 11:51:38 -0400

    test

--- test/fabricators/fabricator.rb
+++ test/fabricators/fabricator.rb
@@ -1,10 +1,10 @@
 Fabricator(:user) do
 	firstname { Faker::Name.first_name }
 	lastname { Faker::Name.last_name }
-  	email { |attrs| "#{Faker::Lorem.characters(10)}@example.com" }
+  	email "#{Faker::Lorem.characters(10)}@example.com" 
   	active true
-  	token { |attrs| "#{Faker::Lorem.characters(10)}" }
-  	facebookid { |attrs| "#{Faker::Number.number(4)}" }
+  	token {Faker::Lorem.characters(10)}
+  	facebookid {Faker::Number.number(4)}
   	smallpic "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/p50x50/75022_10
 152135585049906_64496435_t.jpg" 
 	mediumpic "https://fbcdn-photos-e-a.akamaihd.net/hphotos-ak-prn1/t1.0-0/75022_101521355
@@ -14,8 +14,8 @@ Fabricator(:user) do
 end
 
 Fabricator(:location) do
-	latitude { |attrs| "#{Faker::Address.latitude}" }
-	longitude { |attrs| "#{Faker::Address.longitude}" }
+	latitude {Faker::Address.latitude}
+	longitude {Faker::Address.longitude}
 	accuracy 40
 	timestamp 1236
 end
@@ -27,5 +27,6 @@ end
 
 Fabricator(:web_test_user, from: :user) do
 	activationqueue_id 0
+	after_create { |attrs| Fabricate(:location, user_id: attrs[:id] )}
 end
 
