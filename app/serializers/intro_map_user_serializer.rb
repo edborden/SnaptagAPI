e79760d9baeaeb08ef0a9dbd17fa57990442763a
Ed Borden <borden.edward@gmail.com>
2014-05-11 15:24:49 -0400

    test

--- app/serializers/intro_map_user_serializer.rb
+++ app/serializers/intro_map_user_serializer.rb
@@ -1,8 +1,8 @@
 class IntroMapUserSerializer < ActiveModel::Serializer
 	attributes :exposed_count
-	has_many :locations
+#	has_many :locations
 
-	def locations
-		object.latest_location
-	end
+#	def locations
+#		object.latest_location
+#	end
 end
