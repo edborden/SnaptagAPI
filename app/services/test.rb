fafa4eeed1ccca596c6c73ba36866b9d99158ce7
Ed Borden <borden.edward@gmail.com>
2014-05-12 22:48:55 -0400

    test

--- app/services/test.rb
+++ app/services/test.rb
@@ -3,7 +3,7 @@ class Test
 	def web
 		ed = User.find_by(lastname: "Borden")
 		queue = Activationqueue.create
-		11.times { |n| instance_variable_set("@user" + (n+25).to_s, Fabricate(:active_test_user, id: (n+25),activationqueue_id: queue.id)) }
-		25.times { |n| instance_variable_set("@user" + (n+125).to_s, Fabricate(:active_test_user, id: (n+125))) }
+		11.times { |n| instance_variable_set("@user" + (n+25).to_s, Fabricate(:active_test_user,activationqueue_id: queue.id)) }
+		25.times { |n| instance_variable_set("@user" + (n+125).to_s, Fabricate(:active_test_user)) }
 	end
 end
