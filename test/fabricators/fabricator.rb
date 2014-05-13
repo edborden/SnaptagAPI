f1bd20ee2671a6090f3d578c64a0da79b810e035
Ed Borden <borden.edward@gmail.com>
2014-05-12 22:09:03 -0400

    test

--- test/fabricators/fabricator.rb
+++ test/fabricators/fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:user) do
   	email "#{Faker::Lorem.characters(10)}@example.com" 
   	active true
   	token {Faker::Lorem.characters(10)}
-  	facebookid {Faker::Number.number(4)}
+  	facebookid {Faker::Number.characters(10)}
   	smallpic "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/p50x50/75022_10
 152135585049906_64496435_t.jpg" 
 	mediumpic "https://fbcdn-photos-e-a.akamaihd.net/hphotos-ak-prn1/t1.0-0/75022_101521355
