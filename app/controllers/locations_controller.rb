e5c0d66756286ca9b073f996cd996ac152882d31
Ed Borden <borden.edward@gmail.com>
2014-05-14 22:13:32 -0400

    location testing

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,7 @@ class LocationsController < ApplicationController
 	def update
 		Location.create_from_client(@current_user,params)
 		@current_user.increment!(:influence)
-		render :ok
+		render status: 200
 	end
 
 end
