ef164e1063c5cea3ca4d52a7114c5668572d49a7
Ed Borden <borden.edward@gmail.com>
2014-05-22 21:49:08 -0400

    demo test

--- app/serializers/inactive_map_user_serializer.rb
+++ app/serializers/inactive_map_user_serializer.rb
@@ -1,7 +1,7 @@
 class InactiveMapUserSerializer < ActiveModel::Serializer
 	attributes :id, :exposed_count
 	has_many :locations
-	has_one :zone, embed :ids, include: true
+	has_one :zone
 
 #	def locations
 #		object.locations.first
