1f69a40659b01139c1de9e218100a18b6a6e3bae
Ed Borden <borden.edward@gmail.com>
2014-05-22 22:26:05 -0400

    demo test

--- app/services/demo.rb
+++ app/services/demo.rb
@@ -18,7 +18,7 @@ class Demo
 
 	def create_active_users_in zone
 		Fabricate.times(30, :user, zone_id:zone.id) do
-			after_create { |attrs| Fabricate(:location, user_id: attrs[:id], lat: (rand*0.02 -0.01) + zone.lat, lon: (rand*0.02 -0.01) + zone.lon}
+			after_create { |attrs| Fabricate(:location, user_id: attrs[:id], lat: (rand*0.02 -0.01) + zone.lat, lon: (rand*0.02 -0.01) + zone.lon)}
 		end
 	end
 	
