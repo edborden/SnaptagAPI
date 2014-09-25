6cea53256c2fce7adadd6a6388fc66a786632d48
Ed Borden <borden.edward@gmail.com>
2014-09-25 13:15:14 -0400

    sessions serializer error

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,4 @@
-class SessionSerializer < ActiveModel::Serializer
+class SessionSerializer < ApplicationSerializer
 	attributes :token
 	has_one :user, embed_in_root: true, serializer: MeSerializer, root: 'user'
 end
