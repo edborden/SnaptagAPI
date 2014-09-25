f765c258140fe727a014d1e5516b178d3bb97a9d
Ed Borden <borden.edward@gmail.com>
2014-09-25 12:45:04 -0400

    serializer error

--- app/serializers/me_serializer.rb
+++ app/serializers/me_serializer.rb
@@ -1,4 +1,4 @@
 class MeSerializer < ApplicationSerializer
 	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :smallpic, :mediumpic, :largepic, :influence
-	has_many :suspects, embed_in_root: true, serializer: SuspectSerializer, root: "user"
+	has_many :suspects, embed_in_root: true, serializer: SuspectSerializer, root: "users"
 end
