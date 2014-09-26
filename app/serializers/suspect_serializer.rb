8a2762d7cd018b0d8d1f3725e076694e81f6f287
Ed Borden <borden.edward@gmail.com>
2014-09-26 15:50:27 -0400

    serializers error

--- app/serializers/suspect_serializer.rb
+++ app/serializers/suspect_serializer.rb
@@ -2,7 +2,8 @@ class SuspectSerializer < ApplicationSerializer
 	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :smallpic, :mediumpic, :largepic, :influence
 	has_many :locations, embed_in_root: true
 
-	def locations
-		scope.targets.include? object ? object.locations : []
-	end
+	# include locations if the user is a target
+	#def locations
+	#	scope.targets.include?(object) ? object.locations : []
+	#end
 end
