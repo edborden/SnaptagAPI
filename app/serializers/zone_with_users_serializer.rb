caa1146087c19b7fc90a876d8586f629514ef7a9
Ed Borden <borden.edward@gmail.com>
2014-09-25 21:17:25 -0400

    filter currentuser from zonewithuserserliazer

--- app/serializers/zone_with_users_serializer.rb
+++ app/serializers/zone_with_users_serializer.rb
@@ -1,4 +1,13 @@
 class ZoneWithUsersSerializer < ApplicationSerializer
 	attributes :lat, :lon, :range, :active
 	has_many :users, embed_in_root: true
+
+	def users
+		if current_user
+			users = object.users -= [current_user]
+		else
+			object.users
+		end
+	end
+
 end
