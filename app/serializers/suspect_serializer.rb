2c6c20b35c9d5e357513ac45ac7f5ee6f0d8409f
Ed Borden <borden.edward@gmail.com>
2014-09-27 15:23:01 -0400

    hunt join

--- app/serializers/suspect_serializer.rb
+++ app/serializers/suspect_serializer.rb
@@ -1,5 +1,5 @@
 class SuspectSerializer < ApplicationSerializer
-	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :smallpic, :mediumpic, :largepic, :influence
+	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :influence#, :smallpic, :mediumpic, :largepic
 	#has_many :locations, embed_in_root: true
 
 	# include locations if the user is a target
