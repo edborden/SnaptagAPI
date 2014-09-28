39c782ebb5fa2c267a3b295d05b5f2214a2ae569
Ed Borden <borden.edward@gmail.com>
2014-09-28 13:14:54 -0400

    hunt controller work

--- app/serializers/suspect_serializer.rb
+++ app/serializers/suspect_serializer.rb
@@ -1,6 +1,6 @@
 class SuspectSerializer < ApplicationSerializer
-	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :influence#, :smallpic, :mediumpic, :largepic
-	#has_many :locations, embed_in_root: true
+	attributes :name, :exposed_count, :counteract_count, :disavowed_count, :compromised_count, :influence, :smallpic, :mediumpic, :largepic
+	has_many :locations, embed_in_root: true
 
 	# include locations if the user is a target
 	#def locations
