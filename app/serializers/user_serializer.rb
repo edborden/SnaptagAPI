47ffe8c21e3ba16bfe4a281096a25d04ac04a1bd
Ed Borden <borden.edward@gmail.com>
2014-10-08 11:04:28 -0400

    user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer < ApplicationSerializer
 	attributes :exposed_count, :activated_at
-	has_one :location, embed_in_root: true
+	has_many :locations, embed_in_root: true
 
-	def location
-		object.locations.last
+	def locations
+		[object.locations.last]
 	end
 end
