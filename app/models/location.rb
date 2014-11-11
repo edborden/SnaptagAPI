cdc96ccc997c26fd09325c4e132d8cd8ca9d526b
Ed Borden <borden.edward@gmail.com>
2014-11-11 15:49:37 -0500

    locations real-time push

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,11 @@
 class Location < ActiveRecord::Base
 	belongs_to :user
 
+	after_create :push_create
+
+	def push_create
+		json_package = LocationSerializer.new self
+		Pusher.trigger(user.id, 'location', json_package)
+	end
+
 end
