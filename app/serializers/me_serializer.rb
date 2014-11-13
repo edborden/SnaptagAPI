6a5494543b8cf8be8bb7a222ac9318c475315dbd
Ed Borden <borden.edward@gmail.com>
2014-11-12 22:58:33 -0500

    me serializer with notifications

--- app/serializers/me_serializer.rb
+++ app/serializers/me_serializer.rb
@@ -1,5 +1,11 @@
 class MeSerializer < ApplicationSerializer
 	attributes :name, :targets_found_count, :found_count, :stalkers_exposed_count, :exposed_count, :stealth, :status, :smallpic, :mediumpic, :largepic
 	has_many :suspects, embed_in_root: true, serializer: SuspectSerializer, root: "users"
+	has_many :notifications, embed_in_root: true
 	has_many :targets
+
+	def notifications
+		object.mailbox.notifications.first(9)
+	end
+		
 end
