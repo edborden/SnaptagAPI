4ddaea49f30caabb02984f5591ae6fd2ed4359e7
Ed Borden <borden.edward@gmail.com>
2014-11-15 14:09:38 -0500

    notifications controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	resources :sessions, only: [:create, :index, :destroy]
 	resources :locations, only: :create
 	resources :zones, only: :index
+	resources :notifications, only: :update
 
 	get 'hunts/join', to: 'hunts#join'
 	get 'hunts/found_target', to: 'hunts#found_target'
