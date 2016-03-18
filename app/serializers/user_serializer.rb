c59a97c5c167f933168332baba856f06ff4e8fad
edborden <borden.edward@gmail.com>
2016-03-18 16:11:46 -0400

    serialize more on user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ApplicationSerializer
-	attributes :targets_found_count, :activated_at
+	attributes :targets_found_count, :activated_at, :name, :smallpic, :largepic
 	has_many :locations, embed_in_root: true
 
 	def locations
