6cc5ee525f7b2c6ca3772ac508efb38d01a33487
edborden <borden.edward@gmail.com>
2016-03-18 16:33:40 -0400

    idiot

--- app/services/webs_hole_filler.rb
+++ app/services/webs_hole_filler.rb
@@ -10,11 +10,11 @@ class WebsHoleFiller
 	end
 
 	def need_givers
-		zone.users.need_givers(@user.id) - [currently_webbed]
+		@zone.users.need_givers(@user.id) - [currently_webbed]
 	end
 
 	def need_receivers
-		zone.users.need_receivers(@user.id) - [currently_webbed]
+		@zone.users.need_receivers(@user.id) - [currently_webbed]
 	end
 
 	def run
